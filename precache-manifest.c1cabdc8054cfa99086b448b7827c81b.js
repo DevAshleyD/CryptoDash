fe985b7d114f0e98e5334e5b2db828a598ec8b36
Ashley Donald <Ashd32@gmail.com>
2019-11-24 20:12:39 +0000

    Updates

---
+++ precache-manifest.c1cabdc8054cfa99086b448b7827c81b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b9b0f81cc496005cd3",
+    "url": "/cryptodash/static/js/runtime~main.58b9b0f8.js"
+  },
+  {
+    "revision": "5b60d7a5b6753a49473b",
+    "url": "/cryptodash/static/js/main.5b60d7a5.chunk.js"
+  },
+  {
+    "revision": "264bfa4a2d936077270e",
+    "url": "/cryptodash/static/js/1.264bfa4a.chunk.js"
+  },
+  {
+    "revision": "5b60d7a5b6753a49473b",
+    "url": "/cryptodash/static/css/main.22e896d1.chunk.css"
+  },
+  {
+    "revision": "d7331fa7fc2f63d7e60e8e52fb1fee0b",
+    "url": "/cryptodash/index.html"
+  }
+];
